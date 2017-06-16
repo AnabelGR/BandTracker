4c7af0acfbe8aea630729168046842049e6f762d
Anabel Ramirez <anabel@apparentinc.com>
2017-06-16 08:28:24 -0700

    add passing first test for venue object.

---
+++ Objects/Venue.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+using BandTracker;
+
+namespace BandTracker.Objects
+{
+  public class Venue
+  {
+    private int _id;
+    private string _name;
+
+  public Venue(string Name, int Id = 0)
+  {
+    _name = Name;
+    _id = Id;
+  }
+
+  public override bool Equals(System.Object otherVenue)
+  {
+    if(!(otherVenue is Venue))
+    {
+      return false;
+    }
+    else
+    {
+      Venue newVenue = (Venue) otherVenue;
+      bool idEquality = (this.GetId() == newVenue.GetId());
+      bool nameEquality = (this.GetName() == newVenue.GetName());
+      return (idEquality && nameEquality);
+    }
+  }
+
+  public int GetId()
+  {
+    return _id;
+  }
+
+  public string GetName()
+  {
+    return _name;
+  }
+
+  public void SetId(int Id)
+  {
+    _id = Id;
+  }
+  public void SetName(string Name)
+  {
+    _name = Name;
+  }
+
+
+  public static List<Venue> GetAll()
+  {
+    List<Venue> allVenues = new List<Venue>{};
+
+    SqlConnection conn = DB.Connection();
+    conn.Open();
+
+    SqlCommand cmd = new SqlCommand("SELECT * FROM venues", conn);
+    SqlDataReader rdr = cmd.ExecuteReader();
+
+    while(rdr.Read())
+    {
+      int courseId = rdr.GetInt32(0);
+      string courseName = rdr.GetString(1);
+
+      Venue newVenue = new Venue(courseName, courseId);
+      allVenues.Add(newVenue);
+    }
+
+    if (rdr != null)
+        {
+          rdr.Close();
+        }
+        if (conn != null)
+        {
+          conn.Close();
+        }
+        return allVenues;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("Delete FROM venues;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
